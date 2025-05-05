a863c65e665bfb6efb591fd8292e6c817fcbab4a
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-05 14:05:13 +0300

    get variable before initialization

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 import pytest
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @pytest.fixture
 def app():
