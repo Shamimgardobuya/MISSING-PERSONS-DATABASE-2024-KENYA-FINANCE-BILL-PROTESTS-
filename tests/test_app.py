a6ccb0dd232abdefa8c36cee223cbe5eba0fcdeb
Shamimgardobuya <shamimobuya@gmail.com>
2025-07-15 08:21:26 +0300

    added test config value in config file

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,12 +8,7 @@ from app import db, create_app
 
 @pytest.fixture
 def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:"),
-        "SQLALCHEMY_TRACK_MODIFICATIONS" : False
-    })
+    app = create_app(config_name="test")
     with app.app_context():
         db.create_all()
         yield app
